4c4d97c7dc3b5464dcbf56b060ecfbd7b2e0d306
AlbertViCa <albert0-v.c@hotmail.com>
2022-06-08 18:20:26 -0500

    Data loader added at main class.

--- src/main/java/sia/tacos/TacoCloudApplication.java
+++ src/main/java/sia/tacos/TacoCloudApplication.java
@@ -1,13 +1,33 @@
 package sia.tacos;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import sia.tacos.domain.data.repositories.interfaces.IngredientRepository;
+import sia.tacos.model.Ingredient;
 
 @SpringBootApplication
 public class TacoCloudApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TacoCloudApplication.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner dataLoader(IngredientRepository ingredientRepository) {
+        return args -> {
+            ingredientRepository.save(Ingredient.builder().id("FLTO").name("Flour Tortilla").type(Ingredient.Type.WRAP).build());
+            ingredientRepository.save(Ingredient.builder().id("COTO").name("Corn Tortilla").type(Ingredient.Type.WRAP).build());
+            ingredientRepository.save(Ingredient.builder().id("GRBF").name("Ground Beef").type(Ingredient.Type.PROTEIN).build());
+            ingredientRepository.save(Ingredient.builder().id("CARN").name("Carnitas").type(Ingredient.Type.PROTEIN).build());
+            ingredientRepository.save(Ingredient.builder().id("TMTO").name("Diced Tomatoes").type(Ingredient.Type.VEGGIES).build());
+            ingredientRepository.save(Ingredient.builder().id("LETC").name("Lettuce").type(Ingredient.Type.VEGGIES).build());
+            ingredientRepository.save(Ingredient.builder().id("CHED").name("Cheddar").type(Ingredient.Type.CHEESE).build());
+            ingredientRepository.save(Ingredient.builder().id("JACK").name("Monterrey Jack").type(Ingredient.Type.CHEESE).build());
+            ingredientRepository.save(Ingredient.builder().id("SLSA").name("Salsa").type(Ingredient.Type.SAUCE).build());
+            ingredientRepository.save(Ingredient.builder().id("SRCR").name("Sour Cream").type(Ingredient.Type.SAUCE).build());
+        };
     }
 
 }
