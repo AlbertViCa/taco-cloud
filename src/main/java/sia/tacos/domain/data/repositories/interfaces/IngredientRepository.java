0c8e13745794bf5edb45b983667b64ed17616264
AlbertViCa <albert0-v.c@hotmail.com>
2022-06-08 18:17:14 -0500

    Explicit method implementations have been eliminated.

--- src/main/java/sia/tacos/domain/data/repositories/interfaces/IngredientRepository.java
+++ src/main/java/sia/tacos/domain/data/repositories/interfaces/IngredientRepository.java
@@ -3,14 +3,5 @@ package sia.tacos.domain.data.repositories.interfaces;
 import org.springframework.data.repository.CrudRepository;
 import sia.tacos.model.Ingredient;
 
-import java.util.Optional;
-
 public interface IngredientRepository extends CrudRepository<Ingredient, String> {
-
-    Iterable<Ingredient> findAll();
-
-    Optional<Ingredient> findById(String id);
-
-    Ingredient save(Ingredient ingredient);
-
 }
