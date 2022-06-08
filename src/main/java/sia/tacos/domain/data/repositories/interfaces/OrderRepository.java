94a10670695b6b5be0fcbbdd1ae43506a6c247e8
AlbertViCa <albert0-v.c@hotmail.com>
2022-06-08 18:19:11 -0500

    Custom querys added.

--- src/main/java/sia/tacos/domain/data/repositories/interfaces/OrderRepository.java
+++ src/main/java/sia/tacos/domain/data/repositories/interfaces/OrderRepository.java
@@ -1,9 +1,19 @@
 package sia.tacos.domain.data.repositories.interfaces;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
-import sia.tacos.model.Taco;
 import sia.tacos.model.TacoOrder;
 
-public interface OrderRepository extends CrudRepository<Taco, Long> {
-    TacoOrder save(TacoOrder order);
+import java.util.Date;
+import java.util.List;
+
+public interface OrderRepository extends CrudRepository<TacoOrder, Long> {
+    List<TacoOrder> findByDeliveryZip(String deliveryZip);
+
+    List<TacoOrder> getAllByDeliveryCityAndPlacedAtBetween(String deliveryCity, Date startDate, Date endDate);
+
+    List<TacoOrder> getOrdersByDeliveryZipAndPlacedAtBetween(String deliveryZip, Date startDate, Date endDate);
+
+    @Query(value = "SELECT o FROM TacoOrder o WHERE o.deliveryCity = 'Seattle'")
+    List<TacoOrder> readOrdersDeliveredInSeattle();
 }
