ee3678df74d194e67534050a5f4a88373947848c
AlbertViCa <albert0-v.c@hotmail.com>
2022-06-08 15:27:16 -0500

    IngredientRepository and OrderRepository now extends CrudRepository meaning that an explicit implementation like JdbcIngredientRepository and JdbcOrderRepository is no longer necessary.

--- src/main/java/sia/tacos/domain/data/repositories/interfaces/IngredientRepository.java
+++ src/main/java/sia/tacos/domain/data/repositories/interfaces/IngredientRepository.java
@@ -1,10 +1,11 @@
 package sia.tacos.domain.data.repositories.interfaces;
 
+import org.springframework.data.repository.CrudRepository;
 import sia.tacos.model.Ingredient;
 
 import java.util.Optional;
 
-public interface IngredientRepository {
+public interface IngredientRepository extends CrudRepository<Ingredient, String> {
 
     Iterable<Ingredient> findAll();
 
--- src/main/java/sia/tacos/domain/data/repositories/interfaces/OrderRepository.java
+++ src/main/java/sia/tacos/domain/data/repositories/interfaces/OrderRepository.java
@@ -1,7 +1,9 @@
 package sia.tacos.domain.data.repositories.interfaces;
 
+import org.springframework.data.repository.CrudRepository;
+import sia.tacos.model.Taco;
 import sia.tacos.model.TacoOrder;
 
-public interface OrderRepository {
+public interface OrderRepository extends CrudRepository<Taco, Long> {
     TacoOrder save(TacoOrder order);
 }
