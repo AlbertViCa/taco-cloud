bdd0410520db22eb5ddf648ec2fd9dd6db4d00f3
AlbertViCa <albert0-v.c@hotmail.com>
2022-06-08 10:55:12 -0500

    IngredientRepository (interface and implementation) added.

---
+++ src/main/java/sia/tacos/domain/data/repositories/interfaces/IngredientRepository.java
@@ -0,0 +1,15 @@
+package sia.tacos.domain.data.repositories.interfaces;
+
+import sia.tacos.model.Ingredient;
+
+import java.util.Optional;
+
+public interface IngredientRepository {
+
+    Iterable<Ingredient> findAll();
+
+    Optional<Ingredient> findById(String id);
+
+    Ingredient save(Ingredient ingredient);
+
+}
