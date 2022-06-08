439a243d48e228782d42e845d10ff8d10e232a25
AlbertViCa <albert0-v.c@hotmail.com>
2022-06-08 15:11:49 -0500

    OrderController makes use of OrderRepository.

--- src/main/java/sia/tacos/web/controllers/OrderController.java
+++ src/main/java/sia/tacos/web/controllers/OrderController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.bind.support.SessionStatus;
+import sia.tacos.domain.data.repositories.interfaces.OrderRepository;
 import sia.tacos.model.TacoOrder;
 
 import javax.validation.Valid;
@@ -17,6 +18,12 @@ import javax.validation.Valid;
 @RequestMapping("/orders")
 @SessionAttributes("tacoOrder")
 public class OrderController {
+
+    private final OrderRepository orderRepo;
+
+    public OrderController(OrderRepository orderRepo) {
+        this.orderRepo = orderRepo;
+    }
 
     @GetMapping("/current")
     public String orderForm() {
@@ -31,17 +38,14 @@ public class OrderController {
      * If there are, the method concludes without processing the <b>Taco</b> and returns the <b>"design"</b> view name so that the form is redisplayed.
      * */
 
-    @PostMapping
-    public String processOrder(
-            @Valid TacoOrder order,
-            Errors errors,
-            SessionStatus sessionStatus) {
 
-        if (errors.hasErrors()) {
+    @PostMapping
+    public String processOrder(@Valid TacoOrder order, Errors errors, SessionStatus sessionStatus) {
+        if(errors.hasErrors()) {
             return "orderForm";
         }
 
-        log.info("Order submitted: {}", order);
+        orderRepo.save(order);
         sessionStatus.setComplete();
 
         return "redirect:/";
