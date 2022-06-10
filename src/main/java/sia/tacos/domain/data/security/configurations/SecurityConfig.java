bdbe33f9b22d52c3a674e99d98a23a28e8e0e3b0
AlbertViCa <albert0-v.c@hotmail.com>
2022-06-10 18:42:30 -0500

    Security capabilities added.

---
+++ src/main/java/sia/tacos/domain/data/security/configurations/SecurityConfig.java
@@ -0,0 +1,58 @@
+package sia.tacos.domain.data.security.configurations;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import sia.tacos.domain.data.repositories.interfaces.UserRepository;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(UserRepository userRepo) {
+        return username -> userRepo.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User '" + username + "' not found"));
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeRequests()
+                    .mvcMatchers("/design", "/orders").hasRole("USER")
+                    .anyRequest().permitAll()
+
+                .and()
+                    .formLogin()
+                        .loginPage("/login")
+
+                .and()
+                    .logout()
+                        .logoutSuccessUrl("/")
+
+                // Make H2-Console non-secured; for debug purposes
+                .and()
+                    .csrf()
+                        .ignoringAntMatchers("/h2-console/**")
+
+                // Allow pages to be loaded in frames from the same origin; needed for H2-Console
+                .and()
+                    .headers()
+                        .frameOptions()
+                            .sameOrigin()
+
+                .and()
+                .build();
+    }
+}
